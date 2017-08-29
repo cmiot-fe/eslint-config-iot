e38a5a5c72600c2caa123ac783343ad42c40db26
snadn <snadn@snadn.cn>
2017-08-29 10:56:36 +0800

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"extends": "airbnb",
+	"rules": {
+		"linebreak-style": ["error", "windows"],
+		"indent": ["error", "tab"],
+		"no-tabs": "off",
+		"import/no-dynamic-require": "off",
+		"no-throw-literal": "off",
+		"max-len": ["warn", 120]
+	}
+}
