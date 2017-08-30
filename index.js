53126e1053faa9cfedd684c2476cfd024ea52d42
snadn <snadn@snadn.cn>
2017-08-30 15:33:39 +0800

    update

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ module.exports = {
 	"rules": {
 		"linebreak-style": ["error", "windows"],
 		"indent": ["error", "tab"],
+		"space-before-function-paren": ["error", {
+			"anonymous": "never",
+			"named": "never",
+			"asyncArrow": "never"
+		}],
+		"no-unused-expressions": ["error", {
+			"allowShortCircuit": true
+		}],
+		"class-methods-use-this": "off",
 		"no-tabs": "off",
 		"no-throw-literal": "off",
 		"no-underscore-dangle": "off",
