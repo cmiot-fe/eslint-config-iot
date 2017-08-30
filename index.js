31313c2c9bfc64938380a294385c1ffccdb48bca
snadn <snadn@snadn.cn>
2017-08-30 14:07:05 +0800

    no-underscore-dangle off

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ module.exports = {
 		"linebreak-style": ["error", "windows"],
 		"indent": ["error", "tab"],
 		"no-tabs": "off",
-		"import/no-dynamic-require": "off",
 		"no-throw-literal": "off",
+		"no-underscore-dangle": "off",
+		"import/no-dynamic-require": "off",
 		"max-len": ["warn", 120]
 	}
 }
