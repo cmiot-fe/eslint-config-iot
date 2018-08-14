bab65a9ef8e7f2db9d81017b3d67ff97dd667b79
snadn <snadn@snadn.cn>
2018-08-14 09:39:19 +0800

    feat: extend cmiot

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# eslint-config-iot
+# eslint-config-iot (deprecated)
 
 基于 airbnb 进行定制的 eslint 规则，具体修改见 [代码](index.js)
--- index.js
+++ index.js
@@ -1,48 +1,10 @@
 module.exports = {
-	"extends": "airbnb",
-	"parser": "babel-eslint",
-	"settings": {
-		"import/resolver": "webpack"
-	},
+	"extends": "cmiot",
 	"rules": {
 		"linebreak-style": ["error", "windows"],
 		"indent": ["error", "tab"],
-		"space-before-function-paren": ["error", {
-			"anonymous": "never",
-			"named": "never",
-			"asyncArrow": "always"
-		}],
-		"no-unused-expressions": ["error", {
-			"allowShortCircuit": true
-		}],
-		"no-unused-vars": ["error", {
-			"vars": "local",
-			"args": "none",
-			"ignoreRestSiblings": true
-		}],
-		"no-bitwise": ["error", {
-			"int32Hint": true
-		}],
 		"no-tabs": "off",
-		"no-throw-literal": "off",
-		"no-underscore-dangle": "off",
-		"import/no-dynamic-require": "off",
-		"import/extensions": ["error", "always", "js", {
-			"ignorePackages": true
-		}],
-		"react/jsx-filename-extension": ["error", {
-			"extensions": [".js", ".jsx"]
-		}],
 		"react/jsx-indent": ["error", "tab"],
 		"react/jsx-indent-props": ["error", "tab"],
-		"react/no-multi-comp": "off",
-		"react/forbid-prop-types": "off",
-		"react/prop-types": "off",
-		"react/no-array-index-key": "warn",
-		"react/no-did-mount-set-state": "off",
-		"jsx-a11y/label-has-for": "off",
-		"global-require": "off",
-		"class-methods-use-this": "off",
-		"max-len": ["warn", 120]
 	}
 }
